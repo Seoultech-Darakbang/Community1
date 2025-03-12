7fb799cece0f2088729513855b03e4b997d6bb41
bebeis <ferrater1013@gmail.com>
2025-03-13 01:03:49 +0900

    fix(login): 로그인 메인 + 로그인 폼 통합

--- src/main/java/darak/community/HomeController.java
+++ src/main/java/darak/community/HomeController.java
@@ -1,14 +1,19 @@
 package darak.community;
 
+import darak.community.domain.member.Member;
+import darak.community.web.argumentresolver.Login;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Slf4j
 @Controller
 public class HomeController {
+
     @GetMapping("/")
-    public String home() {
+    public String home(@Login Member member, Model model) {
+        model.addAttribute("member", member);
         return "home";
     }
 }
