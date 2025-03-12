15b08490b1cec237640353c53363fbb416f4aa0c
bebeis <ferrater1013@gmail.com>
2025-03-12 15:15:53 +0900

    feat(board): 게시판 상위 카테고리 추가

--- src/main/java/darak/community/domain/BoardCategory.java
+++ src/main/java/darak/community/domain/BoardCategory.java
@@ -1,23 +1,18 @@
 package darak.community.domain;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 
 @Entity
 @Getter
-public class Board {
+public class BoardCategory {
+
     @Id
     @GeneratedValue
     private Long id;
 
     private String name;
 
-    private String description;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    private BoardCategory boardCategory;
 }
