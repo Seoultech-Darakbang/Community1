e8047393eaf4cb9772c77a0894859f1bcaf5369e
Junseo Bae <ferrater1013@gmail.com>
2025-03-13 22:41:39 +0900

    refactor(testData):

--- src/main/java/darak/community/TestDataInit.java
+++ src/main/java/darak/community/TestDataInit.java
@@ -1,6 +1,8 @@
 package darak.community;
 
 import darak.community.domain.member.Member;
+import darak.community.service.BoardCategoryService;
+import darak.community.service.BoardService;
 import darak.community.service.MemberService;
 import java.time.LocalDate;
 import lombok.RequiredArgsConstructor;
@@ -12,9 +14,15 @@ import org.springframework.boot.ApplicationRunner;
 public class TestDataInit implements ApplicationRunner {
 
     private final MemberService memberService;
+    private final BoardCategoryService boardCategoryService;
+    private final BoardService boardService;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
+        addMemberData();
+    }
+
+    private void addMemberData() {
         Member testMember = Member.builder()
                 .loginId("test")
                 .name("테스트")
