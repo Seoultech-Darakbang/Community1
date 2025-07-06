974ff86536d69876720ba4b47eedd5f6c9f365b8
Junseo Bae <ferrater1013@gmail.com>
2025-07-06 23:29:08 +0900

    refactor: Aspect가 처리 범위 개선
    클래스 레벨도 처리 가능하도록 개선하였습니다.

--- src/main/java/darak/community/core/aop/ServiceAuthAspect.java
+++ src/main/java/darak/community/core/aop/ServiceAuthAspect.java
@@ -19,28 +19,35 @@ public class ServiceAuthAspect {
 
     private final ServiceAuthChecker serviceAuthChecker;
 
-    @Before("execution(* darak.community.service..*.*(..)) && @annotation(darak.community.core.auth.ServiceAuth)")
+    @Before("execution(* darak.community.service..*.*(..)) && (@annotation(darak.community.core.auth.ServiceAuth) || @within(darak.community.core.auth.ServiceAuth))")
     public void authCheck(JoinPoint joinPoint) {
         log.info("{} 에서 권한 체크 진행", joinPoint.getSignature());
 
-        // ThreadLocal에서 현재 로그인한 사용자 정보 조회
         Long currentMemberId = UserContext.getCurrentMemberId();
 
         if (currentMemberId == null) {
             throw new IllegalStateException("로그인되지 않은 사용자입니다.");
         }
 
-        // 메서드에서 @Auth 어노테이션 정보 추출
         MethodSignature signature = (MethodSignature) joinPoint.getSignature();
-        ServiceAuth serviceAuth = signature.getMethod().getAnnotation(ServiceAuth.class);
+        ServiceAuth serviceAuth = getServiceAuth(joinPoint, signature);
 
         if (serviceAuth == null) {
-            throw new IllegalStateException("@Auth 어노테이션을 찾을 수 없습니다.");
+            throw new IllegalStateException("@ServiceAuth 어노테이션을 찾을 수 없습니다.");
         }
 
         if (!serviceAuthChecker.isMemberGradeOrHigher(currentMemberId, serviceAuth.value())) {
-            throw new IllegalStateException("접근할 수 없는 권한의 메서드를 호출하였습니다");
+            throw new IllegalStateException("접근할 수 없는 권한의 메서드를 호출하였습니다: " + serviceAuth.message());
+        }
+    }
+
+    private ServiceAuth getServiceAuth(JoinPoint joinPoint, MethodSignature signature) {
+        ServiceAuth methodAuth = signature.getMethod().getAnnotation(ServiceAuth.class);
+        if (methodAuth != null) {
+            return methodAuth;
         }
 
+        Class<?> targetClass = joinPoint.getTarget().getClass();
+        return targetClass.getAnnotation(ServiceAuth.class);
     }
 }
